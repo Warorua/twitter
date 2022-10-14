69f4fd50f35a8b00b6551453b9036ec7daa0c7ab
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-14 22:35:26 +0300

    upload image capsule fix

--- includes/elements/tweet_form.php
+++ includes/elements/tweet_form.php
@@ -17,67 +17,72 @@
             <textarea class="form-control bg-transparent border-0 px-0 h-125px" id="kt_social_feeds_post_input" name="text" data-kt-autosize="true" rows="1" placeholder="Type your message..."></textarea>
             <!--end::Input-->
             <div class="row g-5 w-100">
-                <!--begin::Image input-->
-                <div class="image-input image-input-outline col-md-5 mb-5" data-kt-image-input="true" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)">
-                    <!--begin::Image preview wrapper-->
-                    <div class="image-input-wrapper w-100 h-125px" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)"></div>
-                    <!--end::Image preview wrapper-->
+                <div class="col-md-6">
+                    <!--begin::Image input-->
+                    <div class="image-input image-input-outline w-100 mb-5" data-kt-image-input="true" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)">
+                        <!--begin::Image preview wrapper-->
+                        <div class="image-input-wrapper w-100 h-125px" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)"></div>
+                        <!--end::Image preview wrapper-->
 
-                    <!--begin::Edit button-->
-                    <label class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Change avatar">
-                        <i class="bi bi-pencil-fill fs-7"></i>
+                        <!--begin::Edit button-->
+                        <label class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Change avatar">
+                            <i class="bi bi-pencil-fill fs-7"></i>
 
-                        <!--begin::Inputs-->
-                        <input type="file" name="image" accept=".png, .jpg, .jpeg" />
-                        <input type="hidden" name="avatar_remove" />
-                        <!--end::Inputs-->
-                    </label>
-                    <!--end::Edit button-->
+                            <!--begin::Inputs-->
+                            <input type="file" name="image" accept=".png, .jpg, .jpeg" />
+                            <input type="hidden" name="avatar_remove" />
+                            <!--end::Inputs-->
+                        </label>
+                        <!--end::Edit button-->
 
-                    <!--begin::Cancel button-->
-                    <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Cancel avatar">
-                        <i class="bi bi-x fs-2"></i>
-                    </span>
-                    <!--end::Cancel button-->
+                        <!--begin::Cancel button-->
+                        <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Cancel avatar">
+                            <i class="bi bi-x fs-2"></i>
+                        </span>
+                        <!--end::Cancel button-->
 
-                    <!--begin::Remove button-->
-                    <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Remove avatar">
-                        <i class="bi bi-x fs-2"></i>
-                    </span>
-                    <!--end::Remove button-->
+                        <!--begin::Remove button-->
+                        <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Remove avatar">
+                            <i class="bi bi-x fs-2"></i>
+                        </span>
+                        <!--end::Remove button-->
+                    </div>
+                    <!--end::Image input-->
                 </div>
-                <!--end::Image input-->
+                <div class="col-md-6">
+                    <!--begin::Image input-->
+                    <div class="image-input image-input-outline w-100 ms-5" data-kt-image-input="true" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)">
+                        <!--begin::Image preview wrapper-->
+                        <div class="image-input-wrapper w-100 h-125px" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)"></div>
+                        <!--end::Image preview wrapper-->
 
-                <!--begin::Image input-->
-                <div class="image-input image-input-outline ms-5 col-md-5" data-kt-image-input="true" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)">
-                    <!--begin::Image preview wrapper-->
-                    <div class="image-input-wrapper w-100 h-125px" style="background-image: url(../assets/media/stock/1600x800/img-3.jpg)"></div>
-                    <!--end::Image preview wrapper-->
+                        <!--begin::Edit button-->
+                        <label class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Change avatar">
+                            <i class="bi bi-pencil-fill fs-7"></i>
 
-                    <!--begin::Edit button-->
-                    <label class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Change avatar">
-                        <i class="bi bi-pencil-fill fs-7"></i>
+                            <!--begin::Inputs-->
+                            <input type="file" name="image_2" accept=".png, .jpg, .jpeg" />
+                            <input type="hidden" name="avatar_remove" />
+                            <!--end::Inputs-->
+                        </label>
+                        <!--end::Edit button-->
 
-                        <!--begin::Inputs-->
-                        <input type="file" name="image_2" accept=".png, .jpg, .jpeg" />
-                        <input type="hidden" name="avatar_remove" />
-                        <!--end::Inputs-->
-                    </label>
-                    <!--end::Edit button-->
+                        <!--begin::Cancel button-->
+                        <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Cancel avatar">
+                            <i class="bi bi-x fs-2"></i>
+                        </span>
+                        <!--end::Cancel button-->
 
-                    <!--begin::Cancel button-->
-                    <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Cancel avatar">
-                        <i class="bi bi-x fs-2"></i>
-                    </span>
-                    <!--end::Cancel button-->
-
-                    <!--begin::Remove button-->
-                    <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Remove avatar">
-                        <i class="bi bi-x fs-2"></i>
-                    </span>
-                    <!--end::Remove button-->
+                        <!--begin::Remove button-->
+                        <span class="btn btn-icon btn-circle btn-color-muted btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" data-bs-dismiss="click" title="Remove avatar">
+                            <i class="bi bi-x fs-2"></i>
+                        </span>
+                        <!--end::Remove button-->
+                    </div>
+                    <!--end::Image input-->
                 </div>
-                <!--end::Image input-->
+
+
             </div>
 
             <!--begin::Option-->
