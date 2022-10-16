c2970ed8c0114f29baa02c40377e77f2bcf64ee8
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-16 03:31:10 +0300

    user points update 2

--- includes/api_config.php
+++ includes/api_config.php
@@ -668,9 +668,9 @@ if($user['access_token'] == ''){
 
 
 if ($user['p_cipher'] == 0) {
-  $user_points = $user['p_value'];
+  $user_points = floatvar($user['p_value']);
 } else {
-  $user_points = safeDecrypt($user['p_value'], $user['p_key']);
+  $user_points = floatvar(safeDecrypt($user['p_value'], $user['p_key']));
 }
 }
 
