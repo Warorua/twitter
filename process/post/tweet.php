9349439b754013dfb711175a68c4d1627390cc31
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-25 19:27:53 +0300

    tweet script update

--- process/post/tweet.php
+++ process/post/tweet.php
@@ -6,7 +6,7 @@ include '../../includes/api_config.php';
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
   $_SESSION['error'] = 'Invalid request';
-  die();
+  die($_SESSION['error']);
  // header('location: ../logo_view.php');
 }
 
@@ -36,6 +36,7 @@ if (isset($_FILES['file']['name'])) {
 
   $media1 = $abraham_client->upload('media/upload', ['media' => $file_path]);
   array_push($_SESSION['tweetMedia'], $media1->media_id_string);
+  unlink($file_path);
 }
 
 
@@ -47,10 +48,14 @@ if (isset($_FILES['file']['name'])) {
 
 if(!isset($_FILES['file']['name'])) {
 
-  if($_POST['text'] != ''){
-    $name = $_POST['text'];
-  }else{
-    $name = '';
+  if (isset($_POST['text'])) {
+    if ($_POST['text'] != '') {
+      $name = $_POST['text'];
+    } else {
+      $name = '';
+    }
+  } else {
+    $name = 'Script test tweet';
   }
 
   if(isset($_POST['tweet_id'])){
@@ -68,9 +73,15 @@ if(isset($_SESSION['tweetMedia'])){
 }
   
 
+if($media == '' && $rep_id == '' && $name == ''){
+  $_SESSION['error'] = 'Invalid: Empty tweet!';
+  die();
+}
 
 
 
+if(isset($_POST['t_topic'])){
+  
   if ($_POST['t_topic'] == 1) {
     $data = $abraham_client->get('trends/place', [
       'id' => '23424863',
@@ -79,13 +90,21 @@ if(isset($_SESSION['tweetMedia'])){
     $t_topic = '';
     foreach ($topic[0]['trends'] as $id => $row) {
       $t_topic .= $row['name'] . ' ';
-      if ($id == 20) {
+      if ($id == 15) {
         break;
       }
     }
   } else {
     $t_topic = '';
   }
+  
+}else{
+  $t_topic = '';
+}
+
+
+
+
 
   $parameters = [
     'status' => $name . ' ' . $t_topic,
@@ -100,7 +119,16 @@ if(isset($_SESSION['tweetMedia'])){
 
   $message = 'You just tweeted successfully!';
   $output =  $message;
-  $_SESSION['success'] = $message;
+
+  $out_sys = array_convert($result);
+  if(isset($out_sys['error'])){
+    $_SESSION['error'] = $out_sys['error'];
+  }elseif(isset($out_sys['errors'])){
+    $_SESSION['error'] = $out_sys['errors'][0]['message'];
+  }else{
+     $_SESSION['success'] = $message;
+  }
+ 
 
   $auth_user = $user['t_id'];
   charge($charge['tweet_charge']);
@@ -108,4 +136,5 @@ if(isset($_SESSION['tweetMedia'])){
   twitter_log($user['email'], '', $status, $mode, $user['id'], $auth_user, $output);
   
   echo json_encode($result);
+ //echo $out_sys['errors'][0]['message'];
 }
