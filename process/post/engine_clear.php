3e3e9cfcda6478ef2786b2eaa88a8008c0ddc8cf
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-25 10:58:38 +0300

    engine clear fixer

--- process/post/engine_clear.php
+++ process/post/engine_clear.php
@@ -1,8 +1,5 @@
 <?php
 include '../../includes/conn.php';
-include '../../includes/session.php';
-require '../../vendor/autoload.php';
-include '../../includes/api_config.php';
 
 if(!isset($_POST['user'])){
     $conn = $pdo->open();
