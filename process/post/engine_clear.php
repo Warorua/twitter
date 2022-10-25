f0f60dce8845d3115e62c574c0bcb5f4c8d566d6
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-25 10:59:52 +0300

    engine clear fixer 2

--- process/post/engine_clear.php
+++ process/post/engine_clear.php
@@ -1,6 +1,21 @@
 <?php
 include '../../includes/conn.php';
 
+
+function time_sub($date, $unit)
+{
+  $a = date_create($date);
+  $b = date_create(date('Y-m-d H:i:s'));
+  $interval = date_diff($b, $a);
+  $hrs = 0;
+  if($interval->format("%d") != '0'){
+    $hrs = $interval->format("%d") * 24;
+  }
+  $mmm = $unit;
+  return $interval->format("%H")+$hrs;
+}
+
+
 if(!isset($_POST['user'])){
     $conn = $pdo->open();
 $stmt = $conn->prepare("SELECT * FROM engine_monitor");
