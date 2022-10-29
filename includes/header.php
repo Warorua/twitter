27de6c9c1215750a818e161785196506f4cf00f8
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-10-30 02:57:40 +0300

    logout fix 2

--- includes/header.php
+++ includes/header.php
@@ -422,7 +422,7 @@
                     <!--end::Menu item-->
                     <!--begin::Menu item-->
                     <div class="menu-item px-5">
-                        <a href="<?php echo $parent_url ?>/includes/logout#" class="menu-link px-5">Sign Out</a>
+                        <a href="<?php echo $parent_url ?>/account/logout#" class="menu-link px-5">Sign Out</a>
                     </div>
                     <!--end::Menu item-->
                 </div>
