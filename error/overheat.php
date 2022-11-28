4602b231e1711fb2e7e1a464712e49ddc8516a41
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-11-28 14:00:38 +0300

    handling 2

--- error/overheat.php
+++ error/overheat.php
@@ -85,7 +85,7 @@ if (isset($_GET['error'])) {
 												<path opacity="0.5" d="M5 5C5 4.44772 5.44772 4 6 4H18C18.5523 4 19 4.44772 19 5V5C19 5.55228 18.5523 6 18 6H6C5.44772 6 5 5.55228 5 5V5Z" fill="currentColor" />
 												<path opacity="0.5" d="M9 4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V4H9V4Z" fill="currentColor" />
 											</svg>
-											Delete
+											Unsubscribe from Active App
 										</span>
 										<!--end::Svg Icon-->
 									</a>
