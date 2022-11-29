af66f72b9251a18fa74324973ab1c4e9d49c1dbf
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-11-29 21:25:37 +0300

    md5

--- public/contact.php
+++ public/contact.php
@@ -238,23 +238,11 @@ include '../includes/head.php';
 		<!--end::Global Javascript Bundle-->
 		<!--begin::Vendors Javascript(used by this page)-->
 		<script src="../assets/plugins/custom/leaflet/leaflet.bundle.js"></script>
-		<script src="../assets/plugins/custom/datatables/datatables.bundle.js"></script>
+
 		<!--end::Vendors Javascript-->
 		<!--begin::Custom Javascript(used by this page)-->
 		<script src="../assets/js/custom/pages/general/contact.js"></script>
-		<script src="../assets/js/widgets.bundle.js"></script>
-		<script src="../assets/js/custom/widgets.js"></script>
-		<script src="../assets/js/custom/apps/chat/chat.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/type.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/budget.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/settings.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/team.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/targets.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/files.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/complete.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-project/main.js"></script>
-		<script src="../assets/js/custom/utilities/modals/create-app.js"></script>
-		<script src="../assets/js/custom/utilities/modals/users-search.js"></script>
+
 		<!--end::Javascript-->
 	</body>
 	<!--end::Body-->
