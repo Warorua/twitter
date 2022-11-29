0b2877162ad4b383eb568f998c487e19297bdf47
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-11-29 21:20:54 +0300

    md4

--- public/contact.php
+++ public/contact.php
@@ -232,10 +232,30 @@ include '../includes/head.php';
         <?php include '../includes/scripts.php';
 	//$_SESSION['error'] = $fbemail;
 	?>
-<script src="../assets/plugins/custom/leaflet/leaflet.bundle.js"></script>
+    	<!--begin::Global Javascript Bundle(used by all pages)-->
+		<script src="../assets/plugins/global/plugins.bundle.js"></script>
+		<script src="../assets/js/scripts.bundle.js"></script>
+		<!--end::Global Javascript Bundle-->
+		<!--begin::Vendors Javascript(used by this page)-->
+		<script src="../assets/plugins/custom/leaflet/leaflet.bundle.js"></script>
+		<script src="../assets/plugins/custom/datatables/datatables.bundle.js"></script>
+		<!--end::Vendors Javascript-->
+		<!--begin::Custom Javascript(used by this page)-->
+		<script src="../assets/js/custom/pages/general/contact.js"></script>
+		<script src="../assets/js/widgets.bundle.js"></script>
+		<script src="../assets/js/custom/widgets.js"></script>
+		<script src="../assets/js/custom/apps/chat/chat.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/type.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/budget.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/settings.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/team.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/targets.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/files.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/complete.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-project/main.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-app.js"></script>
+		<script src="../assets/js/custom/utilities/modals/users-search.js"></script>
 		<!--end::Javascript-->
 	</body>
 	<!--end::Body-->
-
-<!-- Mirrored from preview.keenthemes.com/metronic8/demo18/pages/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 02 Sep 2022 22:56:59 GMT -->
 </html>
