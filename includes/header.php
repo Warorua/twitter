13553b3a5d89978fb244747242ee3829fd8e21b9
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-12-20 13:42:00 +0000

    menu 1

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
             <!--begin::Wrapper-->
             <div class="flex-grow-1">
                 <!--begin::Aside toggle-->
-                <button class="btn btn-icon btn-color-gray-800 btn-active-color-primary aside-toggle justify-content-start w-30px w-lg-40px" id="kt_aside_toggle">
+                <button class="btn btn-icon btn-dark btn-color-gray-800 btn-active-color-primary aside-toggle justify-content-start w-30px w-lg-40px" id="kt_aside_toggle">
                     <!--begin::Svg Icon | path: icons/duotune/general/gen059.svg-->
                     <span class="svg-icon svg-icon-2">
                         <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -14,7 +14,7 @@
                             <rect opacity="0.3" y="12" width="8" height="3" rx="1.5" fill="currentColor" />
                             <rect opacity="0.3" width="12" height="3" rx="1.5" fill="currentColor" />
                         </svg>
-                    </span>Menu
+                    </span> Menu
                     <!--end::Svg Icon-->
                 </button>
                 <!--end::Aside toggle-->
