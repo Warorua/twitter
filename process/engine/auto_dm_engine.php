fca985ae36a4988641d1a5f5f33e8de5340bba6d
Warorua <waroruaalex640@gmail.com>
2022-12-21 05:16:22 +0300

    err9

--- process/engine/auto_dm_engine.php
+++ process/engine/auto_dm_engine.php
@@ -3,8 +3,8 @@ ini_set('max_execution_time', 1800);
 include '../../includes/conn.php';
 
 ////////////Automation tester
-$stmt = $conn->prepare("INSERT INTO tester (slot) VALUES (:slot)");
-$stmt->execute(['slot' => time()]);
+//$stmt = $conn->prepare("INSERT INTO tester (slot) VALUES (:slot)");
+//$stmt->execute(['slot' => time()]);
 /////////////////////////////
 
 $stmt = $conn->prepare("SELECT *  FROM auto_dm WHERE time<:time LIMIT 10");
