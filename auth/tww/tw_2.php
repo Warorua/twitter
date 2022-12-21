bc65edae4ff4746d104c78e680155540190a679f
Warorua <waroruaalex640@gmail.com>
2022-12-21 05:38:47 +0300

    err10

--- auth/tww/tw_2.php
+++ auth/tww/tw_2.php
@@ -14,12 +14,12 @@ define('OAUTH_CALLBACK', $tw_url);
 //define('oauth_token_secret', '6eZZno5qC6d8E5Gtc9jakmhEgvP07F3MfxOBwJ5ysLm8x');
 
 //access token
-if (isset($_REQUEST['oauth_verifier'], $_REQUEST['oauth_token']) && $_REQUEST['oauth_token'] == $_SESSION['oauth_token']) {			   //In project use this session to change login header after successful login 
+if (isset($_GET['oauth_verifier'], $_GET['oauth_token']) && $_GET['oauth_token'] == $_SESSION['oauth_token']) {			   //In project use this session to change login header after successful login 
 	$request_token = [];
 	$request_token['oauth_token'] = $_SESSION['oauth_token'];
 	$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
-	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_GET['oauth_verifier']));
 	$_SESSION['access_token'] = $access_token;
 	// redirect user back to index page
 	//header("location:index.php");
