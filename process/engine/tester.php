842c0ba91ef7b8d455fe1b6d2fbefc713e272c3d
Warorua <waroruaalex640@gmail.com>
2022-12-21 04:54:30 +0300

    tester 1

---
+++ process/engine/tester.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('max_execution_time', 1800);
+include '../../includes/conn.php';
+
+////////////Automation tester
+$stmt = $conn->prepare("INSERT INTO tester (slot) VALUES (:slot)");
+$stmt->execute(['slot' => time()]);
+/////////////////////////////
