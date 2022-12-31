92ec3eb50c35fb73f07225c03d1042b9cd11078c
Warorua <waroruaalex640@gmail.com>
2022-12-31 04:13:16 +0300

    dgjm

--- process/engine/tester.php
+++ process/engine/tester.php
@@ -1,8 +1,2 @@
 <?php
-//ini_set('max_execution_time', 1800);
-include '../../includes/conn.php';
-
-////////////Automation tester
-//$stmt = $conn->prepare("INSERT INTO tester (slot) VALUES (:slot)");
-//$stmt->execute(['slot' => time()]);
-/////////////////////////////
+header('location:https://kotnova.com/process/engine/market_process.php');
