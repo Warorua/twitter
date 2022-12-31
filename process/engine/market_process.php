19497bcd12045b2474f32405276d9a3331329d05
Warorua <waroruaalex640@gmail.com>
2022-12-31 15:09:52 +0300

    dm extract account

--- process/engine/market_process.php
+++ process/engine/market_process.php
@@ -25,7 +25,7 @@ $ffl = [
 }
 
 
-$followers = $bird_elephant->user('citizentvkenya')->followers($ffl);
+$followers = $bird_elephant->user('Miss_Prresident')->followers($ffl);
 //*/
 
 $data = array_convert($followers);
